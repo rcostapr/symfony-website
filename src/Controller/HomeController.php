722c2524440444b4b3c6dec6c439b9e0f70550fc
Rui Costa <ruicosta.pr@gmail.com>
2021-09-23 00:24:33 +0100

    Twig Ready

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Twig\Twig;
 
 class HomeController
 {
@@ -13,7 +14,98 @@ class HomeController
     public function index()
     {
 
-        $html = "<h1>Index page</h1>";
+        $navigation = [
+            1 => [
+                "href" => "/",
+                "caption" => "Home",
+            ],
+            2 => [
+                "href" => "/produtos",
+                "caption" => "Produtos",
+            ],
+            3 => [
+                "href" => "/artigos",
+                "caption" => "Artigos",
+            ],
+
+        ];
+        $twig = Twig::getInstance();
+        $html = $twig->render(
+            "default.html.twig",
+            [
+                "page" => "Home",
+                "a_variable" => var_dump($twig),
+                "navigation" => $navigation,
+            ]
+        );
+        $response = new Response($html);
+        return $response;
+    }
+
+    /**
+     * @Route("/artigos")
+     */
+    public function artigos()
+    {
+
+        $navigation = [
+            1 => [
+                "href" => "/",
+                "caption" => "Home",
+            ],
+            2 => [
+                "href" => "/produtos",
+                "caption" => "Produtos",
+            ],
+            3 => [
+                "href" => "/artigos",
+                "caption" => "Artigos",
+            ],
+
+        ];
+        $twig = Twig::getInstance();
+        $html = $twig->render(
+            "default.html.twig",
+            [
+                "page" => "Artigos",
+                "a_variable" => var_dump($twig),
+                "navigation" => $navigation,
+            ]
+        );
+        $response = new Response($html);
+        return $response;
+    }
+
+    /**
+     * @Route("/produtos")
+     */
+    public function produtos()
+    {
+
+        $navigation = [
+            1 => [
+                "href" => "/",
+                "caption" => "Home",
+            ],
+            2 => [
+                "href" => "/produtos",
+                "caption" => "Produtos",
+            ],
+            3 => [
+                "href" => "/artigos",
+                "caption" => "Artigos",
+            ],
+
+        ];
+        $twig = Twig::getInstance();
+        $html = $twig->render(
+            "default.html.twig",
+            [
+                "page" => "Produtos",
+                "a_variable" => var_dump($twig),
+                "navigation" => $navigation,
+            ]
+        );
         $response = new Response($html);
         return $response;
     }
--- src/Twig/Twig.php
+++ src/Twig/Twig.php
@@ -1,16 +1,31 @@
 <?php
 
-namespace Twig;
+namespace App\Twig;
 
 use Twig\Loader\FilesystemLoader;
 
+/**
+ * Twig Template
+ */
 class Twig
 {
+    private static $instance;
+    /**
+     * New Instance
+     */
     public static function new()
     {
-        $loader = new FilesystemLoader('/path/to/templates');
+        $loader = new FilesystemLoader('../src/views');
         $twig = new \Twig\Environment($loader, [
-            'cache' => '/path/to/compilation_cache',
+            'cache' => '../cache',
         ]);
+        return $twig;
+    }
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = self::new();
+        }
+        return self::$instance;
     }
 }
