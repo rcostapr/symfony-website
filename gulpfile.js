d96dbc1a92ecb051fc64305b4db7ee88ab0b04b6
Rui Costa <ruicosta.pr@gmail.com>
2021-10-30 16:21:21 +0100

    Update gulp source

--- gulpfile.js
+++ gulpfile.js
@@ -60,160 +60,185 @@ function phpServer(done) {
 
 // Clean vendor
 function clean() {
-    return del(["./public/js/vendor/", "./public/css/vendor/", "./css/vendor/", "./js/vendor/", "./scss/vendor/"]);
+    return del(["./public/js/vendor/", "./public/css/vendor/", "./css/vendor/", "./js/vendor/", "./scss/vendor/", "./css/*.min.css", "./public/css/*.min.css"]);
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-    // Bootstrap JS
-    var bootstrapJS = gulp
-        .src("./node_modules/bootstrap/dist/js/*")
-        .pipe(gulp.dest("./js/vendor/bootstrap"));
-    // Bootstrap SCSS
-    var bootstrapSCSS = gulp
-        .src("./node_modules/bootstrap/scss/**/*")
-        .pipe(gulp.dest("./scss/vendor/bootstrap"));
-
-    // ChartJS
-    var chartJS = gulp
-        .src("./node_modules/chart.js/dist/*.js")
-        .pipe(gulp.dest("./js/vendor/chartjs/chart.js"));
-
-    // Toastr
-    var toastr = gulp
-        .src("./node_modules/toastr/build/*.js")
-        .pipe(gulp.dest("./js/vendor/toastr"));
-    // Toastr SCSS
-    var toastrSCSS = gulp
-        .src("./node_modules/toastr/*.scss")
-        .pipe(gulp.dest("./scss/vendor/toastr"));
-
-    // Sweetalert2
-    var sweetalert2 = gulp
-        .src("./node_modules/sweetalert2/dist/*.js")
-        .pipe(gulp.dest("./js/vendor/sweetalert2"));
-    // Sweetalert SCSS
-    var sweetalertSCSS = gulp
-        .src("./node_modules/sweetalert2/src/*.scss")
-        .pipe(gulp.dest("./scss/vendor/sweetalert2"));
-    // Sweetalert2 SCSS
-    var sweetalert2SCSS = gulp
-        .src("./node_modules/sweetalert2/src/scss/*")
-        .pipe(gulp.dest("./scss/vendor/sweetalert2/scss"));
-
-    // Select2
-    var select2 = gulp
-        .src("./node_modules/select2/dist/js/*.js")
-        .pipe(gulp.dest("./js/vendor/select2/select2"));
-    // Select2 SCSS
-    var select2SCSS = gulp
-        .src("./node_modules/select2/src/scss/**/*")
-        .pipe(gulp.dest("./scss/vendor/select2/scss"));
-    // Select2 css
-    var select2CSS = gulp
-        .src(
-            "./node_modules/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css"
-        )
-        .pipe(gulp.dest("./css/vendor/select2"));
-
-    // Perfect Scrollbar
-    var perfectScrollbarCSS = gulp
-        .src("./node_modules/perfect-scrollbar/css/perfect-scrollbar.css")
-        .pipe(gulp.dest("./css/vendor/perfectScrollbar"));
-    var perfectScrollbar = gulp
-        .src("./node_modules/perfect-scrollbar/dist/*")
-        .pipe(gulp.dest("./js/vendor/perfect-scrollbar"));
-
-    // DROPZONE
-    var dropzoneCSS = gulp
-        .src("./node_modules/dropzone/dist/min/dropzone.min.css")
-        .pipe(gulp.dest("./css/vendor/dropzone"));
-
-    var dropzone = gulp
-        .src("./node_modules/dropzone/dist/min/dropzone.min.js")
-        .pipe(gulp.dest("./js/vendor/dropzone"));
-
-    // dataTables
-    var dataTables = gulp
-        .src([
-            "./node_modules/datatables.net/js/*.js",
-            "./node_modules/datatables.net-bs4/js/*.js",
-        ])
-        .pipe(gulp.dest("./js/vendor/datatables"));
-
-    // dataTables CSS
-    var dataTablesCss = gulp
-        .src([
-            "./node_modules/datatables.net-bs4/css/*.css",
-        ])
-        .pipe(gulp.dest("./css/vendor/datatables"));
-
-    // Font Awesome
-    var fontAwesome = gulp
-        .src("./node_modules/@fortawesome/fontawesome-free/css/*")
-        .pipe(gulp.dest("./css/vendor/fortawesome"));
-
-    // Font Awesome WebFont
-    var fontAwesomeWebFont = gulp
-        .src("./node_modules/@fortawesome/fontawesome-free/webfonts/*")
-        .pipe(gulp.dest("./public/css/webfonts"));
 
     // jQuery
     var jquery = gulp
         .src([
             "./node_modules/jquery/dist/*",
             "!./node_modules/jquery/dist/core.js",
+            "!./node_modules/jquery/dist/*.slim.*",
+            "!./node_modules/jquery/dist/*.min.*",
         ])
         .pipe(gulp.dest("./js/vendor/jquery"));
 
     // jQuery UI
     var jqueryui = gulp
         .src([
-            "./node_modules/jquery-ui-dist/jquery-ui.min.js",
+            "./node_modules/jquery-ui-dist/jquery-ui.js",
         ])
         .pipe(gulp.dest("./js/vendor/jqueryui"));
     // jQuery UI CSS
     var jqueryuiCss = gulp
         .src([
-            "./node_modules/jquery-ui-dist/jquery-ui.min.css",
-            "./node_modules/jquery-ui-dist/jquery-ui.theme.min.css",
+            "./node_modules/jquery-ui-dist/jquery-ui.css",
+            "./node_modules/jquery-ui-dist/jquery-ui.theme.css",
         ])
         .pipe(gulp.dest("./css/vendor/jqueryui"));
     // jQuery UI IMAGES
-    var jqueryuimage = gulp
+    var jqueryuiImage = gulp
         .src(["./node_modules/jquery-ui-dist/images/*"])
         .pipe(gulp.dest("./public/css/vendor/images"));
 
 
+    // Bootstrap JS
+    var bootstrapJS = gulp
+        .src("./node_modules/bootstrap/dist/js/bootstrap.bundle.js")
+        .pipe(gulp.dest("./js/vendor/bootstrap"));
+
+    var bootstrapCSS = gulp
+        .src("./node_modules/bootstrap/scss/**/*")
+        .pipe(gulp.dest("./scss/vendor/bootstrap"));
+
+    // Font Awesome
+    var fontAwesomeCSS = gulp
+        .src("./node_modules/@fortawesome/fontawesome-free/css/*")
+        .pipe(gulp.dest("./css/vendor/fortawesome"));
+    var fontAwesomeWebFonts = gulp
+        .src("./node_modules/@fortawesome/fontawesome-free/webfonts/*")
+        .pipe(gulp.dest("./public/css/webfonts"));
+
+    // ChartJS
+    var chartJS = gulp
+        .src(["./node_modules/chart.js/dist/Chart.bundle.js", "./node_modules/chartjs-plugin-annotation-fix/chartjs-plugin-annotation.js"])
+        .pipe(gulp.dest("./js/vendor/chartjs"));
+
+    var chartCSS = gulp
+        .src("./node_modules/chart.js/dist/Chart.css")
+        .pipe(gulp.dest("./js/vendor/chartjs"));
+
+    // Toastr
+    var toastrJS = gulp
+        .src("./node_modules/toastr/toastr.js")
+        .pipe(gulp.dest("./js/vendor/toastr"));
+    var toastrCSS = gulp
+        .src("./node_modules/toastr/toastr.scss")
+        .pipe(gulp.dest("./scss/vendor/toastr"));
+
+    // Sweetalert2
+    var sweetalert2JS = gulp
+        .src("./node_modules/sweetalert2/dist/sweetalert2.all.js")
+        .pipe(gulp.dest("./js/vendor/sweetalert2"));
+    var sweetalert2CSS1 = gulp
+        .src("./node_modules/sweetalert2/src/*.scss")
+        .pipe(gulp.dest("./scss/vendor/sweetalert2"));
+    var sweetalert2CSS2 = gulp
+        .src("./node_modules/sweetalert2/src/scss/*")
+        .pipe(gulp.dest("./scss/vendor/sweetalert2/scss"));
+
+    // Select2
+    var select2JS = gulp
+        .src("./node_modules/select2/dist/js/select2.full.js")
+        .pipe(gulp.dest("./js/vendor/select2"));
+    var select2CSS1 = gulp
+        .src("./node_modules/select2/src/scss/**/*")
+        .pipe(gulp.dest("./scss/vendor/select2"));
+    var select2CSS2 = gulp
+        .src(
+            "./node_modules/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.css"
+        )
+        .pipe(gulp.dest("./css/vendor/select2"));
+
+    // Perfect Scrollbar
+    var perfectScrollbarJS = gulp
+        .src("./node_modules/perfect-scrollbar/dist/perfect-scrollbar.js")
+        .pipe(gulp.dest("./js/vendor/perfect-scrollbar"));
+    var perfectScrollbarCSS = gulp
+        .src("./node_modules/perfect-scrollbar/css/perfect-scrollbar.css")
+        .pipe(gulp.dest("./css/vendor/perfectScrollbar"));
+
+    // DROPZONE
+    var dropzoneJS = gulp
+        .src("./node_modules/dropzone/dist/dropzone.js")
+        .pipe(gulp.dest("./js/vendor/dropzone"));
+    var dropzoneCSS = gulp
+        .src("./node_modules/dropzone/dist/dropzone.css")
+        .pipe(gulp.dest("./css/vendor/dropzone"));
+
+    // dataTables
+    var datatablesJS = gulp
+        .src([
+            "./node_modules/datatables.net/js/*.js",
+            "!./node_modules/datatables.net/js/*min*",
+            "./node_modules/datatables.net-bs4/js/*.js",
+            "!./node_modules/datatables.net-bs4/js/*min*",
+        ])
+        .pipe(gulp.dest("./js/vendor/datatables"));
+    var datatablesCSS = gulp
+        .src([
+            "./node_modules/datatables.net-bs4/css/*.css",
+        ])
+        .pipe(gulp.dest("./css/vendor/datatables"));
+
+
     return merge(
-        bootstrapJS,
-        bootstrapSCSS,
-        chartJS,
-        dataTables,
-        dataTablesCss,
-        fontAwesome,
-        fontAwesomeWebFont,
         jquery,
         jqueryui,
         jqueryuiCss,
-        jqueryuimage,
-        toastr,
-        toastrSCSS,
-        sweetalert2,
-        sweetalertSCSS,
-        sweetalert2SCSS,
-        select2,
-        select2SCSS,
-        select2CSS,
-        dropzone,
-        dropzoneCSS,
+        jqueryuiImage,
+        bootstrapJS,
+        bootstrapCSS,
+        fontAwesomeCSS,
+        fontAwesomeWebFonts,
+        chartJS,
+        chartCSS,
+        toastrJS,
+        toastrCSS,
+        sweetalert2JS,
+        sweetalert2CSS1,
+        sweetalert2CSS2,
+        select2JS,
+        select2CSS1,
+        select2CSS2,
+        perfectScrollbarJS,
         perfectScrollbarCSS,
-        perfectScrollbar,
+        dropzoneJS,
+        dropzoneCSS,
+        datatablesJS,
+        datatablesCSS
     );
 }
 
+// JS Vendor Concat task ORDERED
+function concatJsVendor() {
+    return gulp
+        .src([
+            "./js/vendor/jquery/jquery.js",
+            "./js/vendor/jqueryui/jquery-ui.js",
+            "./js/vendor/bootstrap/bootstrap.bundle.js",
+            "./js/vendor/toastr/toastr.js",
+            "./js/vendor/sweetalert2/sweetalert2.all.js",
+            "./js/vendor/select2/select2.full.js",
+            "./js/vendor/perfect-scrollbar/perfect-scrollbar.js",
+            "./js/vendor/dropzone/dropzone.js",
+            "./js/vendor/datatables/dataTables.bootstrap4.js",
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('blundle.js'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("./public/js/vendor"));
+}
+
 // SCSS task
-function scss() {
+function compileScssVendor() {
     return gulp
         .src(["./scss/vendor/**/*.scss"])
         .pipe(plumber())
@@ -229,20 +254,30 @@ function scss() {
                 cascade: false,
             })
         )
-        .pipe(gulp.dest("./css/vendor"))
+        .pipe(gulp.dest("./css/vendor"));
+}
+
+// CSS concat task
+function concatCssVendor() {
+    return gulp
+        .src([
+            "./css/vendor/**/*.css",
+            "!./css/vendor/**/*.min.css",
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('blundle.css'))
         .pipe(
             rename({
                 suffix: ".min",
             })
         )
         .pipe(cleanCSS())
-        .pipe(gulp.dest("./css/vendor"))
-        .pipe(browsersync.stream());
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/css/vendor'));
 }
 
-// TODO
 // SCSS Website Style
-function style() {
+function compileScssSytle() {
     return gulp
         .src(["./scss/*.scss"])
         .pipe(plumber())
@@ -269,39 +304,12 @@ function style() {
         .pipe(browsersync.stream());
 }
 
-// CSS concat task
-function css() {
-    return gulp
-        .src([
-            "./css/vendor/jqueryui/jquery-ui.min.css",
-            "./css/vendor/jqueryui/jquery-ui.theme.min.css",
-            "./css/vendor/bootstrap/bootstrap.min.css",
-            "./css/vendor/fortawesome/all.min.css",
-            "./css/vendor/toastr/toastr.min.css",
-            "./css/vendor/sweetalert2/sweetalert2.min.css",
-            "./css/vendor/select2/select2-bootstrap4.min.css",
-            "./css/vendor/perfectScrollbar/perfect-scrollbar.css",
-            "./css/vendor/dropzone/dropzone.min.css",
-            "./css/vendor/datatables/dataTables.bootstrap4.min.css"
-
-        ])
-        .pipe(sourcemaps.init())
-        .pipe(concat('blundle.css'))
-        .pipe(
-            rename({
-                suffix: ".min",
-            })
-        )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/css/vendor'))
-        .pipe(browsersync.stream());
-}
-
 
 // JS task
-function js() {
+function compileLibrary() {
     return gulp
         .src(["./js/library.js"])
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(
             header(banner, {
@@ -313,56 +321,31 @@ function js() {
                 suffix: ".min",
             })
         )
-        .pipe(gulp.dest("./public/js"))
-        .pipe(browsersync.stream());
-}
-
-
-// JS Vendor Concat task
-function vendorjs() {
-    return gulp
-        .src([
-            "./js/vendor/jquery/jquery.min.js",
-            "./js/vendor/jqueryui/jquery-ui.min.js",
-            "./js/vendor/bootstrap/bootstrap.bundle.min.js",
-            "./js/vendor/toastr/toastr.min.js",
-            "./js/vendor/sweetalert2/sweetalert2.all.min.js",
-            "./js/vendor/select2/select2/select2.full.min.js",
-            "./js/vendor/perfect-scrollbar/perfect-scrollbar.min.js",
-            "./js/vendor/dropzone/dropzone.min.js",
-            //"./js/vendor/datatables/dataTables.bootstrap4.min.js",
-        ])
-        .pipe(sourcemaps.init())
-        .pipe(concat('blundle.js'))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("./public/js/vendor"))
+        .pipe(gulp.dest("./public/js"))
         .pipe(browsersync.stream());
 }
 
 // Watch files
 function watchFiles() {
-    gulp.watch("./scss/*", gulp.series(gulp.parallel(scss, style)));
-    gulp.watch(["./js/*"], gulp.series("js"));
+    gulp.watch("./scss/*", gulp.series(compileScssSytle));
+    gulp.watch(["./js/*"], gulp.series(compileLibrary));
     gulp.watch("./src/**/*", browserSyncReload);
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const compile = gulp.series(gulp.parallel(scss, style));
-const build = gulp.series(vendor, compile, gulp.parallel(css, js, vendorjs));
-const watch = gulp.series(build, gulp.parallel(watchFiles, phpServer, browserSync));
+const vendor = gulp.series(modules, concatJsVendor, compileScssVendor, concatCssVendor);
+const build = gulp.series(compileScssSytle, compileLibrary);
+const compile = gulp.series(clean, gulp.parallel(vendor, build));
+const watch = gulp.parallel(watchFiles, phpServer, browserSync);
 
 
 // Export tasks
-exports.scss = scss;
-//exports.css = css;
-exports.js = js;
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
+exports.compile = compile;
 exports.watch = watch;
 exports.default = build;
+
+
